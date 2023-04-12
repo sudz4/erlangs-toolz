8275e84643485220957fc82d70c697cb34b9e74f
Matt Sutherland <mwsuther@syr.edu>
2023-04-11 17:37:29 -0700

    clocks working

--- main_clocks_2.py
+++ main_clocks_2.py
@@ -27,20 +27,31 @@ city_tzones = {
 def get_local_time(tz_name):
     timezone = tz.gettz(tz_name)
     local_time = datetime.datetime.now(timezone)
-    return local_time.strftime('%Y-%m-%d %H:%M:%S')
+    return local_time.strftime('%A %Y-%m-%d\n%H:%M:%S') 
+    # %A is the weedkday character
 
 def update_time():
     for i, (city, tz_name) in enumerate(city_tzones.items()):
-        time_labels[i]['text'] = f"{city}: {get_local_time(tz_name)}"
+        time_labels[i]['text'] = get_local_time(tz_name)
     root.after(1000, update_time)  # schedule the next update in 1000 ms (1 second)
 
+# configure UI
 root = tk.Tk()
 root.title("World Clocks")
+root.configure(bg='#2D2D2D')  # set the window background color
 
 time_labels = []
-for city, tz_name in city_tzones.items():
-    time_label = tk.Label(root, text=f"{city}: {get_local_time(tz_name)}", font=("Helvetica", 14))
-    time_label.pack(pady=5)
+
+for index, (city, tz_name) in enumerate(city_tzones.items()):
+    frame = tk.Frame(root, bg='#2D2D2D', highlightbackground='white', highlightthickness=1)
+    frame.grid(row=index // 2, column=index % 2, padx=10, pady=10)
+    
+    city_label = tk.Label(frame, text=city, font=("Arial", 32, "bold"), fg='white', bg='#2D2D2D')
+    city_label.pack(pady=(10, 0))
+    
+    time_label = tk.Label(frame, text=get_local_time(tz_name), font=("Arial", 24), fg='white', bg='#2D2D2D')
+    time_label.pack(pady=(0, 10))
+    
     time_labels.append(time_label)
 
 update_time()  # start updating the time
