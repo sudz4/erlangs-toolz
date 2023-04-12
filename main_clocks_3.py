a3e6ceac58ac7309eed6d59dc4bb5f7acc5fc5a4
Matt Sutherland <mwsuther@syr.edu>
2023-04-11 18:03:03 -0700

    signature add

--- main_clocks_3.py
+++ main_clocks_3.py
@@ -6,7 +6,6 @@
 import tkinter as tk
 import datetime
 from dateutil import tz
-import tkinter.font as tkfont # for dynamic re-sizing of fonts
 
 # city names and time zones
 city_tzones = {
@@ -28,59 +27,80 @@ city_tzones = {
 def get_local_time(tz_name):
     timezone = tz.gettz(tz_name)
     local_time = datetime.datetime.now(timezone)
-    return local_time.strftime('%A %Y-%m-%d\n%H:%M:%S') 
-    # %A is the weedkday character
+    return local_time.strftime('%A %Y-%m-%d\n%H:%M:%S')
 
 def update_time():
     for i, (city, tz_name) in enumerate(city_tzones.items()):
         time_labels[i]['text'] = get_local_time(tz_name)
-    root.after(1000, update_time)  # schedule the next update in 1000 ms (1 second)
+    root.after(1000, update_time)
 
-def resize_text(event):
-    width = root.winfo_width()
-    base_width = 800  # Base window width
-    ratio = width / base_width
-    
-    for time_label in time_labels:
-        current_font = time_label.cget('font')
-        font = tkfont.Font(font=current_font)  # Create a Font object using the correct import
-        font_family = font.actual('family')
-        original_size = font.actual('size')
-        style = font.actual('weight')
-        
-        new_size = int(float(original_size) * ratio)
-        new_font = f"{font_family} {new_size} {style}"
-        time_label.configure(font=new_font)
-
-# UI config, auto resize window w/custom resizing behavior
-# the custom resizing behavior listens for window size changes
-# then it adjusts the font size accordingly.
 root = tk.Tk()
 root.title("World Clocks")
-root.configure(bg='#2D2D2D')  # set the window background color
+root.configure(bg='#15202b')
 
 time_labels = []
 
 for index, (city, tz_name) in enumerate(city_tzones.items()):
-    frame = tk.Frame(root, bg='#2D2D2D', highlightbackground='white', highlightthickness=1)
-    frame.grid(row=index // 2, column=index % 2, padx=10, pady=10)
+    frame = tk.Frame(root, bg='#15202b', highlightbackground='#657786', highlightthickness=1)
+    frame.grid(row=index // 2, column=index % 2, padx=20, pady=20, sticky="nsew")
     
-    city_label = tk.Label(frame, text=city, font=("Arial", 32, "bold"), fg='white', bg='#2D2D2D')
+    city_label = tk.Label(frame, text=city, font=("Arial", 32, "bold"), fg='#ffffff', bg='#15202b')
     city_label.pack(pady=(10, 0))
     
-    time_label = tk.Label(frame, text=get_local_time(tz_name), font=("Arial", 24), fg='white', bg='#2D2D2D')
+    time_label = tk.Label(frame, text=get_local_time(tz_name), font=("Arial", 24), fg='#ffffff', bg='#15202b')
     time_label.pack(pady=(0, 10))
     
     time_labels.append(time_label)
 
-#auto-resizing configuration
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
+ascii_art = """
+ASCII ART
+HEY
+"""
 
-#bind the resize event to the custom resizing behavior
-root.bind('<Configure>', resize_text)
+ascii_art_frame = tk.Frame(root, bg='#15202b')
+ascii_art_frame.grid(row=6, column=1, padx=20, pady=20, sticky="nsew")
+ascii_art_label = tk.Label(ascii_art_frame, text=ascii_art, font=("Courier", 20), fg='#ffffff', bg='#15202b')
+ascii_art_label.pack()
 
-update_time()  #start updating the time
-root.mainloop()  #this runs the Tkinter event loop
+for i in range(3):
+    root.grid_columnconfigure(i, weight=1, uniform="col")
+    root.grid_rowconfigure(i, weight=1, uniform="row")
+
+update_time()
+root.mainloop()
+
+# def get_local_time(tz_name):
+#     timezone = tz.gettz(tz_name)
+#     local_time = datetime.datetime.now(timezone)
+#     return local_time.strftime('%A %Y-%m-%d\n%H:%M:%S')
+
+# def update_time():
+#     for i, (city, tz_name) in enumerate(city_tzones.items()):
+#         time_labels[i]['text'] = get_local_time(tz_name)
+#     root.after(1000, update_time)
+
+# root = tk.Tk()
+# root.title("World Clocks")
+# root.configure(bg='#15202b')  #set the window background color
+
+# time_labels = []
+
+# for index, (city, tz_name) in enumerate(city_tzones.items()):
+#     frame = tk.Frame(root, bg='#15202b', highlightbackground='#657786', highlightthickness=1)
+#     frame.grid(row=index // 2, column=index % 2, padx=20, pady=20, sticky="nsew")
+    
+#     city_label = tk.Label(frame, text=city, font=("Arial", 32, "bold"), fg='#ffffff', bg='#15202b')
+#     city_label.pack(pady=(10, 0))
+    
+#     time_label = tk.Label(frame, text=get_local_time(tz_name), font=("Arial", 24), fg='#ffffff', bg='#15202b')
+#     time_label.pack(pady=(0, 10))
+    
+#     time_labels.append(time_label)
+
+# #set up row and column configurations outside of the loop
+# for i in range(2):
+#     root.grid_columnconfigure(i, weight=1, uniform="col")
+#     root.grid_rowconfigure(i, weight=1, uniform="row")
+
+# update_time()
+# root.mainloop()
