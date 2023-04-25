0918deeb92119bb48872a419559e4a6d405bc645
Matt Sutherland <mwsuther@syr.edu>
2023-04-25 13:53:19 -0700

    work together

--- usd_torreros_id.py
+++ usd_torreros_id.py
@@ -1,7 +1,12 @@
+
+#libs
 import cv2
 import mediapipe as mp
 import numpy as np
 import os
+
+
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
