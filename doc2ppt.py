3bba2c828b0589b1ef3a0957d48afdac57bbfb3c
Matt Sutherland <mwsuther@syr.edu>
2023-04-24 17:36:58 -0700

    main

--- doc2ppt.py
+++ doc2ppt.py
@@ -25,3 +25,22 @@ def create_slide(prs, title, content):
     text_frame = content_shape.text_frame
     text_frame.text = content
 
+# the main function to run the program
+def main():
+    word_file = 'doc2ppt_file.docx'
+    ppt_file = 'doc2_ppt_slides.pptx'
+
+    data = read_word_doc(word_file)
+    prs = Presentation()
+
+    for index, item in enumerate(data):
+        if index % 2 == 0:
+            title = item
+        else:
+            content = item
+            create_slide(prs, title, content)
+    
+    prs.save(ppt_file)
+
+if __name__ == "__main__":
+    main()
